f2b410442d035b3af5fa58f5095f36404982aec4
KaiYang13 <151112680+KaiYang13@users.noreply.github.com>
2024-01-27 15:57:50 +0800

    Add files via upload

---
+++ dogehub/adopt.php
@@ -0,0 +1,79 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="Styles/adopt.css">
+  <link rel="stylesheet" href="Styles/header.css">
+  <title>Adopt - DogeHub</title>
+</head>
+<body>
+<header>
+  <div class="logo">
+    <a href="homeadmin.php">
+      <img src="Images/dogehub_logo.png" alt="DogeHub Logo" id="logo">
+    </a>
+  </div>
+
+  <div class="search">
+    <div class="searchbar">
+      <input type="text" placeholder="Search">
+    </div>
+    <div class="search-icon">
+      <img src="Images/search.png" alt="Search Icon" id="search">
+    </div>
+  </div>
+
+  <nav class="navigations">
+    <ul class="nav_links">
+      <li><a href="about.php"><button>About Us</button></a></li>
+      <li><a href="adopt.php"><button class="adopt">Adopt</button></a></li>
+      <li><a href="donate.php"><button>Donate</button></a></li>
+    </ul>
+  </nav>
+  <div class="calltoactions">
+    <a class="cta" href="logout.php"><button>Log Out</button></a>
+  </div>
+</header>
+
+<div class="adoption-frame">
+  <h1>Adopt a Dog</h1>
+  <p>Find your perfect furry friend and give them a forever home.</p>
+</div>
+
+<div class="dog-card-container">
+  <div class="dog-card">
+    <img src="Images/beagle.png" alt="Dog Image">
+    <h2></h2>
+    <p>Breed: Breed Name</p>
+    <p>Age: 19 years</p>
+    <p>Gender: Male</p>
+    <button class="adopt-button">Adopt Now</button>
+  </div>
+
+  <div class="dog-card">
+    <img src="Images/beagle.png" alt="Dog Image">
+    <h2></h2>
+    <p>Breed: Breed Name</p>
+    <p>Age: 5 years</p>
+    <p>Gender: Female</p>
+    <button class="adopt-button">Adopt Now</button>
+  </div>
+
+  <div class="dog-card">
+    <img src="Images/beagle.png" alt="Dog Image">
+    <h2></h2>
+    <p>Breed: Breed Name</p>
+    <p>Age: 2 years</p>
+    <p>Gender: Male</p>
+    <button class="adopt-button">Adopt Now</button>
+  </div>
+</div>
+
+</body>
+</html>
+
+
+</body>
+</html>
